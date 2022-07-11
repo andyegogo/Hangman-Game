51f1d0efbb9062b373c8a7f27c26dd7ad7c8b661
ANDY EGOGO-STANLEY <150405043@live.unilag.edu.ng>
2022-07-11 11:57:40 +0100

    The clear() function from replit was changed to suite the python console on most IDEs.

--- main.py
+++ main.py
@@ -4,44 +4,45 @@ import hangman_words
 import os
 
 
+def clear_console():
+    command = 'cls'
+    os.system(command)
+
+
 print(hangman_art.logo)
 
-
 chosen_word = random.choice(hangman_words.word_list)
-#print(f'Pssst, the solution is {chosen_word}.')
+# print(f'Pssst, the solution is {chosen_word}.')
 
-display = []
+
 display = ["_" for _ in range(len(chosen_word))]
 print(f"Your guess so far: {' '.join(display)}\n")
 lives = 6
 print(hangman_art.stages[lives])
 
-
 while lives > 0 and "_" in display:
-  guess = input("Guess a letter: ").lower()
-  os.system('cls')
-  print(hangman_art.logo)
-  NOTUGCU = chosen_word.count(guess)
-  if display.count(guess) > 0:
-    print("You already picked this letter. Pick something else\n")
-  elif NOTUGCU > 0:
-    id = -1
-    i = 0
-    while i < NOTUGCU:
-      id = chosen_word.index(guess, id+1)
-      display[id] = chosen_word[id]
-      i+=1
-    print(hangman_art.stages[lives])
-    print(f"Your guess so far: {' '.join(display)}.\n\n")
-  else:
-    lives -= 1
-    print(hangman_art.stages[lives])
-    print(f"Your guess so far: {' '.join(display)}.\n\n")
-    print(f"The letter you picked is not in the word.\nYou have {6-lives} strikes and {lives} more chances\n\n")
-
+    guess = input("Guess a letter: ").lower()
+    clear_console()
+    print(hangman_art.logo)
+    no_times_user_guess_comes_up = chosen_word.count(guess)
+    if display.count(guess) > 0:
+        print("You already picked this letter. Pick something else\n")
+    elif no_times_user_guess_comes_up > 0:
+        id = -1
+        i = 0
+        while i < no_times_user_guess_comes_up:
+            id = chosen_word.index(guess, id + 1)
+            display[id] = chosen_word[id]
+            i += 1
+        print(hangman_art.stages[lives])
+        print(f"Your guess so far: {' '.join(display)}.\n\n")
+    else:
+        lives -= 1
+        print(hangman_art.stages[lives])
+        print(f"Your guess so far: {' '.join(display)}.\n\n")
+        print(f"The letter you picked is not in the word.\nYou have {6 - lives} strikes and {lives} more chances\n\n")
 
 if display.count("_") == 0 or "_" not in display:
-  print("You guessed all the letters correctly.\nYou win!!! 🏆")
+    print("You guessed all the letters correctly.\nYou win!!! 🏆")
 elif lives == 0:
-  print(f"You are out of lives. You lose 👎.\nThe word is '{chosen_word}'")
-  
+    print(f"You are out of lives. You lose 👎.\nThe word is '{chosen_word}'")
